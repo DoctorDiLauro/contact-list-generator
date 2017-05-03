cb6880f631bcbdcfeb61a654a8e9cc9b8ff8eef8
DoctorDiLauro <chrisdilauro@Chriss-MacBook-Pro-3.local>
2017-05-03 12:26:11 -0400

    fixed small errors

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+var peeps = [
+  {
+  name: 'Mercury Man',
+  email: 'theman@mercur.com',
+  tel: '+05551234',
+  loc: [46001200, 69816900]
+},
+{
+  name: 'Venus Venutrer',
+  email: 'venus@theadcenturer.com',
+  tel: '+1551234',
+  loc: [107477000, 108939000]
+},
+{
+  name: 'Jupiter Juggernaut',
+  email: 'thejugger@jupiter.com',
+  tel: '+55551234',
+  loc: [816040000, 740550000]
+}
+];
